ffdb8a05de6628c6e150ef338e0c549ba995f24f
Dmitriy Zavodnov <dmitrii.zavodnov@mail.ru>
2023-09-06 01:01:14 +0300

    added test

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Ольга Курбацкая on 6.09.23.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
